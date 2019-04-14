b7eefba9f863bd6ad10cd1b060abebb277f3c59f
flyingbird2 <buhui.08@163.com>
2019-04-14 18:53:53 +0800

    money对象反序列化

--- src/main/java/com/qihongfei/simpleresttemplate/SimpleResttemplateApplication.java
+++ src/main/java/com/qihongfei/simpleresttemplate/SimpleResttemplateApplication.java
@@ -1,5 +1,6 @@
 package com.qihongfei.simpleresttemplate;
 
+import com.qihongfei.simpleresttemplate.model.Coffee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -10,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -20,32 +23,40 @@ import java.net.URI;
 @Slf4j
 public class SimpleResttemplateApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 //		SpringApplication.run(SimpleResttemplateApplication.class, args);
-		new SpringApplicationBuilder()
-				.sources(SimpleResttemplateApplication.class)
-				.bannerMode(Banner.Mode.OFF)
-				.web(WebApplicationType.NONE)
-				.run(args);
+        new SpringApplicationBuilder()
+                .sources(SimpleResttemplateApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .web(WebApplicationType.NONE)
+                .run(args);
 
-	}
+    }
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder){
-		return builder.build();
-	}
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
-	@Autowired
-	private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		//URI uri=URI.create("https://www.baidu.com");
-        URI uri = UriComponentsBuilder.fromUriString("https://www.baidu.com").build().toUri();
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        //URI uri=URI.create("https://www.baidu.com");
+        URI uri = UriComponentsBuilder.fromUriString("http://www.baidu.com").build().toUri();
         ResponseEntity<String> forEntity = restTemplate.getForEntity(uri, String.class);
-		log.info(forEntity.getBody());
+        log.info(forEntity.getBody());
+
+        URI nameUri = UriComponentsBuilder.fromUriString("http://localhost:8080/coffee/?name={name}")
+                .build("qihongfei");
+        RequestEntity requestEntity = RequestEntity.get(nameUri)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .build();
+        ResponseEntity<Coffee> exchange = restTemplate.exchange(requestEntity, Coffee.class);
+        log.info("exchange response : {}", exchange);
 
 
-	}
+    }
 }
